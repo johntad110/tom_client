1463043a401fe12a9a310f46c03ea1c8a36a1e94
John Tad <johntad110@gmail.com>
2025-08-05 16:25:31 +0300

    test: remove local https cert

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import mkcert from 'vite-plugin-mkcert'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    mkcert(),
     tailwindcss(),
     nodePolyfills(),
   ],
