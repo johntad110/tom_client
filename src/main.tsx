5615a54e5146938b60d90d70f9595969ba6ee84a
John Tad <johntad110@gmail.com>
2025-08-05 18:15:13 +0300

    add telegram analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
+import TelegramAnalytics from '@telegram-apps/analytics'
+
+TelegramAnalytics.init({
+  token: 'eyJhcHBfbmFtZSI6InRoZV9vcGVuX21hcmtldCIsImFwcF91cmwiOiJodHRwczovL3QubWUvb3BuX21rdF9ib3QiLCJhcHBfZG9tYWluIjoiaHR0cHM6Ly90b20tY2xpZW50LnZlcmNlbC5hcHAifQ==!0tuxk2oZO/1xNbLG4dSWtUwDBT8PsQt2puUG1mWl9GY=',
+  appName: 'the_open_market',
+});
 
 const queryClient = new QueryClient();
 const manifestUrl = 'https://raw.githubusercontent.com/johntad110/tom_client/refs/heads/master/public/tonconnect-manifest.json';
